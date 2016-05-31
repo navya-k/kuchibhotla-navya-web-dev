d82d8e7b630f28d2516824122aa36b64076b481c
Navya Kuchibhotla <kuchibhotla.n@husky.neu.edu>
2016-05-30 20:21:20 -0400

    assignment3 : changes made for user and website

---
+++ public/assignment/app.js
@@ -0,0 +1,4 @@
+// IIFE (immediately invoked function expression
+(function(){
+    angular.module("WebAppMaker", ["ngRoute"]);
+})();
---
+++ public/assignment/config.js
@@ -0,0 +1,43 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl : "views/home.html"
+            })
+            .when("/login", {
+                templateUrl : "views/user/login.view.client.html",
+                controller : "LoginController",
+                controllerAs : "model"
+            })
+            .when("/register", {
+                templateUrl : "views/user/register.view.client.html"
+            })
+            .when("/user/:id", {
+                templateUrl : "views/user/profile.view.client.html",
+                controller: "ProfileController",
+                controllerAs: "model"
+            })
+            .when("/user/:userId/website", {
+                templateUrl : "views/website/website-list.view.client.html",
+                controller: "WebsiteListController",
+                controllerAs: "model"
+            })
+            .when("/user/:userId/website/new", {
+                templateUrl: "views/website/website-new.view.client.html",
+                controller: "NewWebsiteController",
+                controllerAs: "model"
+            })
+            .when("/user/:userId/website/:websiteId", {
+                templateUrl: "views/website/website-edit.view.client.html",
+                controller: "EditWebsiteController",
+                controllerAs: "model"
+            })
+            .otherwise ({
+                redirectTo : "/"
+            });
+    }
+})();
